564e054fb204db79fd858d855d04e9cbd42d2508
phenriquecunha <phcs-12@hotmail.com>
2022-10-10 10:29:05 -0300

    Initial structure

---
+++ src/main/java/learning/eejl/EejlApplication.java
@@ -0,0 +1,13 @@
+package learning.eejl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EejlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EejlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learning/eejl/EejlApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.eejl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EejlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
